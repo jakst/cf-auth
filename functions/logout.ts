406a64c2576ab685fd568ca3c57d951c9313d13d
Jakob Norlin <jakob.stahl91@gmail.com>
2022-08-15 15:28:40 +0200

    Apply the same fix for logout

--- functions/logout.ts
+++ functions/logout.ts
@@ -2,11 +2,16 @@ import { serialize } from 'cookie'
 
 export const onRequestPost: PagesFunction = async ({ request }) => {
   const url = new URL(request.url)
-  const response = Response.redirect(url.origin + '/login')
-  response.headers.set(
-    'Set-Cookie',
-    serialize('AUTH_COOKIE', '', { expires: new Date(Date.now() - 1000) })
-  )
 
-  return response
+  const headers = new Headers({
+    Location: url.origin,
+    ['Set-Cookie']: serialize('AUTH_COOKIE', '', {
+      expires: new Date(Date.now() - 1000),
+    }),
+  })
+
+  return new Response(null, {
+    status: 302,
+    headers,
+  })
 }
